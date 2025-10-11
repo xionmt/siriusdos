1d9f87dd1d9bd7f238723a06cbb30dc262e99c2c
Alexander Nicholi <me@nicholatian.com>
2025-10-11 19:05:11 -0400

    group mkdirs together

--- util/fetchdist.bat
+++ util/fetchdist.bat
@@ -2,9 +2,9 @@
 mkdir data
 mkdir data\system
 mkdir data\local
+mkdir data\system\PACIFICC
 curl -fsSL "http://cowlark.com/old-compilers/pacific/pacific751.zip" > data\pacific7.zip
 7z e data\pacific7.zip -odata -aoa *.exe
-mkdir data\system\PACIFICC
 7z e data\pacific.exe -odata\system\PACIFICC -r -aoa -tlzh
 rename data\system\PACIFICC\PPD.INI PPDINI.BAK
 copy src\ppd.ini data\system\PACIFICC\PPD.INI
