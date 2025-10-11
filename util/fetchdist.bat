4e868c548c9c7f56c8110ce8b1d5b7278761eabb
Alexander Nicholi <me@nicholatian.com>
2025-10-11 17:41:28 -0500

    Add mkdir steps for DOSBox image and settings copy step

--- util/fetchdist.bat
+++ util/fetchdist.bat
@@ -1,5 +1,10 @@
 @ECHO OFF
+mkdir data
+mkdir data\system
+mkdir data\local
 curl -fsSL "http://cowlark.com/old-compilers/pacific/pacific751.zip" > data\pacific7.zip
 7z e data\pacific7.zip -odata -aoa *.exe
-mkdir data\system\pacificc
-7z e data\pacific.exe -odata\system\pacificc -r -aoa -tlzh
+mkdir data\system\PACIFICC
+7z e data\pacific.exe -odata\system\PACIFICC -r -aoa -tlzh
+rename data\system\PACIFICC\PPD.INI PPDINI.BAK
+copy src\ppd.ini data\system\PACIFICC\PPD.INI
