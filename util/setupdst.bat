b528167a7ff6748ad22c71de640d54f24609a705
Alexander Nicholi <me@nicholatian.com>
2025-10-11 21:07:45 -0400

    split in case downloads get wonky

---
+++ util/setupdst.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+mkdir data\system
+mkdir data\local
+mkdir data\system\PACIFICC
+
+7z e data\pacific7.zip -odata -aoa *.exe
+7z e data\pacific.exe -odata\system\PACIFICC -r -aoa -tlzh
+rename data\system\PACIFICC\PPD.INI PPDINI.BAK
+copy src\ppd.ini data\system\PACIFICC\PPD.INI
