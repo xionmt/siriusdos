4857760e140275111dd53b4c61feee25e91213bf
Alexander Nicholi <me@nicholatian.com>
2025-10-11 21:06:31 -0400

    grouping

--- util/fetchdst.bat
+++ util/fetchdst.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
+
 mkdir data
 mkdir data\system
 mkdir data\local
 mkdir data\system\PACIFICC
+
 curl -fsSL "http://cowlark.com/old-compilers/pacific/pacific751.zip" > data\pacific7.zip
+
 7z e data\pacific7.zip -odata -aoa *.exe
 7z e data\pacific.exe -odata\system\PACIFICC -r -aoa -tlzh
 rename data\system\PACIFICC\PPD.INI PPDINI.BAK
