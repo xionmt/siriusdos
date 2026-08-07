eb4c950d27709ccf6e17d3af5999abb2500c4c0d
Alexander Nicholi <me@nicholatian.com>
2026-08-07 19:35:52 -0400

    fixup extraction mapping errors

--- util/setupdst.bat
+++ util/setupdst.bat
@@ -5,8 +5,8 @@ MKDIR data\local
 MKDIR data\system\PACIFICC
 
 7Z e data\pacific7.zip -odata -aoa *.exe
-7Z e data\pacific.exe -odata\system\PACIFICC -r -aoa -tlzh
-RENAME data\system\PACIFICC\PPD.INI PPDINI.BAK
-COPY src\ppd.ini data\system\PACIFICC\PPD.INI
+7Z x data\pacific.exe -odata\system\PACIFICC -r -aoa -tlzh
+RENAME data\system\PACIFICC\BIN\PPD.INI PPDINI.BAK
+COPY src\ppd.ini data\system\PACIFICC\BIN\PPD.INI
 COPY src\ansicol.bat data\system\ANSICOL.BAT
 COPY src\senuscol.bat data\system\SENUSCOL.BAT
--- util/setupdst.sh
+++ util/setupdst.sh
@@ -5,8 +5,8 @@ mkdir data/system;
 mkdir data/system/PACIFICC;
 
 7z e data/pacific7.zip -odata -aoa *.exe;
-7z e data/pacific.exe -odata/system/PACIFICC -r -aoa -tlzh;
-mv data/system/PACIFICC/PPD.INI data/system/PACIFICC/PPDINI.BAK;
-cp -a src/ppd.ini data/system/PACIFICC/PPD.INI;
+7z x data/pacific.exe -odata/system/PACIFICC -r -aoa -tlzh;
+mv data/system/PACIFICC/BIN/PPD.INI data/system/PACIFICC/BIN/PPDINI.BAK;
+cp -a src/ppd.ini data/system/PACIFICC/BIN/PPD.INI;
 cp -a src/ansicol.bat data/system/ANSICOL.BAT;
 cp -a src/senuscol.bat data/system/SENUSCOL.BAT;
